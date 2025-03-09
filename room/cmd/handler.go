ed6fca40c66cae3e1e2909661bed347d99d84401
ssvikash_20 <arisivikash@gmail.com>
2025-03-09 14:23:42 +0530

    Rewrite logic to load the current code to a new joniee

--- room/cmd/handler.go
+++ room/cmd/handler.go
@@ -16,7 +16,6 @@ import (
 )
 
 var roomConns = make(map[string][]*websocket.Conn)
-var changes = make(map[string][]string)
 
 func handleConnections(w http.ResponseWriter, r *http.Request) {
 	ws, err := upgrader.Upgrade(w, r, nil)
@@ -48,21 +47,29 @@ func handleConnections(w http.ResponseWriter, r *http.Request) {
 		room, message := s[0], s[1]
 
 		if message == "load" {
-			if len(changes[room]) > 0 {
-				history, _ := json.Marshal(changes[room])
-				if err := ws.WriteMessage(websocket.TextMessage, history); err != nil {
-					fmt.Println("write error:", err)
+			if len(roomConns) > 0 {
+				if err := roomConns[roomID][0].WriteMessage(websocket.TextMessage, []byte("lll")); err != nil {
+					fmt.Println(err)
 					return
 				}
-				continue
 			}
 		}
 
-		rcns := roomConns[room]
+		if room == "lll" {
+			codeResponse := CodeResponse{
+				Code: message,
+			}
 
-		if message != "load" {
-			changes[roomID] = append(changes[roomID], message)
+			for _, v := range roomConns[roomID] {
+				if err := v.WriteMessage(websocket.TextMessage, []byte("done~"+codeResponse.Code)); err != nil {
+					fmt.Println(err)
+					return
+				}
+			}
+			continue
 		}
+
+		rcns := roomConns[room]
 
 		for _, conn := range rcns {
 			if ws != conn {
@@ -82,9 +89,6 @@ type CodeResponse struct {
 func generate(w http.ResponseWriter, r *http.Request) {
 	prompt := r.URL.Query().Get("prompt")
 	langauge := r.URL.Query().Get("language")
-	roomID := r.URL.Query().Get("id")
-
-	changes[roomID] = changes[roomID][:0]
 
 	cmd := exec.Command("python3", "../codellama/generate.py", prompt, langauge)
 
@@ -109,12 +113,9 @@ func generate(w http.ResponseWriter, r *http.Request) {
 }
 
 func debugCode(w http.ResponseWriter, r *http.Request) {
-	roomID := r.URL.Query().Get("id")
 	language := r.URL.Query().Get("language")
 
 	var codeData CodeResponse
-
-	changes[roomID] = changes[roomID][:0]
 
 	if err := json.NewDecoder(r.Body).Decode(&codeData); err != nil {
 		http.Error(w, "Invalid JSON", http.StatusBadRequest)
