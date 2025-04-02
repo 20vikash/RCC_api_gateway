e355521f36617a167a413cfac136e9abcec5bd72
ssvikash_20 <arisivikash@gmail.com>
2025-04-02 13:36:28 +0530

    Connect to a channel

--- internal/mq/rmq_server.go
+++ internal/mq/rmq_server.go
@@ -19,9 +19,12 @@ func failOnError(err error, msg string) {
 	}
 }
 
-func (mq *MQ) ConnectToMq() *amqp.Connection {
+func (mq *MQ) ConnectToMq() *amqp.Channel {
 	conn, err := amqp.Dial(fmt.Sprintf("amqp://%s:%s@viksync_mq:%s/", mq.User, mq.Pass, mq.Port))
 	failOnError(err, "Failed to connect to RabbitMQ")
 
-	return conn
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+
+	return ch
 }
